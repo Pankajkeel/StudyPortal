b5e4a0b495e65fdf77ac324615b4d9d227cd8f04
Pankajkeel <pankaj@getkeel.com>
2022-06-13 08:48:53 +0530

    added signup flow

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 
-import { css } from '@emotion/css';
-
-const container = css`
-    background: yellow;
-    min-height: 100px;
-    min-width: 200px;
-`
 
 const HomeView = ()=>{
-    return (
-        <div className={container}>Hellow world</div>
-    )
+    return <>
+        <p>Home Page</p>
+    </>
 }
 
 export default HomeView;
--- shared/renderer.js
+++ shared/renderer.js
@@ -1,15 +1,15 @@
-import createEmotionServer from '@emotion/server/create-instance'
-import createCache from '@emotion/cache'
+// import createEmotionServer from '@emotion/server/create-instance'
+// import createCache from '@emotion/cache'
 
-const key = 'custom'
-const cache = createCache({ key })
+// const key = 'custom'
+// const cache = createCache({ key })
 
-export const renderStatic = async (html) => {
-  if (html === undefined) {
-    throw new Error('did you forget to return html from renderToString?')
-  }
-  const { extractCritical } = createEmotionServer(cache)
-  const { ids, css } = extractCritical(html)
+// export const renderStatic = async (html) => {
+//   if (html === undefined) {
+//     throw new Error('did you forget to return html from renderToString?')
+//   }
+//   const { extractCritical } = createEmotionServer(cache)
+//   const { ids, css } = extractCritical(html)
 
-  return { html, ids, css }
-}
+//   return { html, ids, css }
+// }
